e863bef18f026242932930f957649e8ae96607e3
dedlop <104914772+dedlop@users.noreply.github.com>
2024-09-21 11:36:39 -0300

    Trazendo informações do banco de dados para a View

--- agenda_pacientes/views.py
+++ agenda_pacientes/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Paciente
 
 def index(request):
     """Página principal da Agenda Avalia"""
     return render(request, 'agenda_pacientes/index.html')
+
+def pacientes(request):
+    """Mostra todos pacientes"""
+    pacientes = Paciente.objects.order_by('date_added')
+    context = {'pacientes': pacientes}
+    return render(request, 'agenda_pacientes/pacientes.html', context)
