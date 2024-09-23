631d90530d99d4f4890404f56655f6ee9b627fbc
dedlop <104914772+dedlop@users.noreply.github.com>
2024-09-23 11:21:51 -0300

    Criado o formulário de cadastro de novos pacientes

--- agenda_pacientes/urls.py
+++ agenda_pacientes/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('pacientes', views.pacientes, name='pacientes'),
+    path('pacientes/<paciente_id>/', views.paciente, name='paciente'),
+    path('novo_paciente', views.novo_paciente, name='novo_paciente'),
 ]
--- agenda_pacientes/views.py
+++ agenda_pacientes/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Paciente
+from .forms import PacienteForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     """Página principal da Agenda Avalia"""
@@ -10,3 +13,25 @@ def pacientes(request):
     pacientes = Paciente.objects.order_by('date_added')
     context = {'pacientes': pacientes}
     return render(request, 'agenda_pacientes/pacientes.html', context)
+
+def paciente(request, paciente_id):
+    """Mostra os dados e horários do paciente"""
+    paciente = Paciente.objects.get(id = paciente_id)
+    dados = paciente.dados_set.order_by('-date_added')
+    context = {'paciente': paciente, 'dados': dados}
+    return render(request, 'agenda_pacientes/paciente.html', context)
+
+def novo_paciente(request):
+    """Adiciona novo paciente"""
+    if request.method != 'POST':
+        # Nenhum dado submetido; cria um formulário em branco
+        form = PacienteForm()
+    else:
+        # Dados de POST submetidos; processa os dados
+        form = PacienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('pacientes'))
+    
+    context = {'form': form}
+    return render(request, 'agenda_pacientes/novo_paciente.html', context)
